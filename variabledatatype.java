a21ce18ad397f8d776561ef9e96d908a222c371b
Aruljothi2 <129252347+Aruljothi2@users.noreply.github.com>
2024-12-27 16:13:48 +0530

    day5

---
+++ variabledatatype.java
@@ -0,0 +1,23 @@
+package corejavaarul;
+
+public class variabledatatype {
+public static void main(String[] args) {
+	String name1="Arul";
+	String name2="Krish";
+	String noun1="clothes";
+	String noun2="box";
+	String noun3="cycle";
+	String noun4="farm";
+	String noun5="glass";
+	String noun6="hill";
+	String adjective1="Busy";
+	String adjective2="Calm";
+	String adjective3="Cool";
+	String place1="school";
+	int number=2020;
+	String verb1="eat";
+
+	String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It is  going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were  protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of  the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned,  "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped  "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year  "+number+", in a world where "+noun6+"s ruled the world.";
+	System.out.println(story);
+}
+}
