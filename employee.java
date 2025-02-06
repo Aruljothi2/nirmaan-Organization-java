f5a0c1772236a3156cefa89f23c01ff9ddb9d9a3
Aruljothi2 <129252347+Aruljothi2@users.noreply.github.com>
2025-02-06 15:53:49 +0530

    oops five employee

---
+++ employee.java
@@ -0,0 +1,53 @@
+package corejavaarul;
+
+public class employee {
+	
+	String name;
+	double salary;
+	long phonenum;
+	
+	public static void main(String[] args) {
+		employee emp1=new employee();
+		emp1.name="krish";
+		emp1.salary=12000;
+		emp1.phonenum=9856742367l;
+		System.out.println("employee name : "+emp1.name);
+		System.out.println("employee salary : "+emp1.salary);
+		System.out.println("employee phonenum : "+emp1.phonenum);
+		
+		employee emp2=new employee();
+		emp2.name="hem";
+		emp2.salary=13000;
+		emp2.phonenum=9856552367l;
+		System.out.println("employee name : "+emp2.name);
+		System.out.println("employee salary : "+emp2.salary);
+		System.out.println("employee phonenum : "+emp2.phonenum);
+		
+		employee emp3=new employee();
+		emp3.name="akash";
+		emp3.salary=11000;
+		emp3.phonenum=9856742367l;
+		System.out.println("employee name : "+emp3.name);
+		System.out.println("employee salary : "+emp3.salary);
+		System.out.println("employee phonenum : "+emp3.phonenum);
+		
+		employee emp4=new employee();
+		emp4.name="vijay";
+		emp4.salary=15000;
+		emp4.phonenum=9856748469l;
+		System.out.println("employee name : "+emp4.name);
+		System.out.println("employee salary : "+emp4.salary);
+		System.out.println("employee phonenum : "+emp4.phonenum);
+		
+		employee emp5=new employee();
+		emp5.name="suki";
+		emp5.salary=22000;
+		emp5.phonenum=8752242367l;
+		System.out.println("employee name : "+emp5.name);
+		System.out.println("employee salary : "+emp5.salary);
+		System.out.println("employee phonenum : "+emp5.phonenum);
+		
+		
+	}
+
+}
